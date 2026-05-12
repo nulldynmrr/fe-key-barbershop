323d35d049900833a5c8a88739286a0e26437537
nulldynmrr <dinarmuhammadakbar04@gmail.com>
2026-05-12 18:02:50 +0700

    feat: add admin dashboard for AI model configuration and exchange rate management

---
+++ src/services/adminService.js
@@ -0,0 +1,31 @@
+import api from "@/utils/request";
+
+export const getAdminProfile = async () => {
+  const res = await api.get("/admin/profile");
+  return res.data;
+};
+
+export const requestAdminOTP = async () => {
+  const res = await api.post("/admin/request-otp");
+  return res.data;
+};
+
+export const updateAdminProfile = async (data) => {
+  const res = await api.put("/admin/profile", data);
+  return res.data;
+};
+
+export const getNotifications = async () => {
+  const res = await api.get("/notifications");
+  return res.data;
+};
+
+export const markNotificationRead = async (id) => {
+  const res = await api.put(`/notifications/${id}/read`);
+  return res.data;
+};
+
+export const markAllNotificationsRead = async () => {
+  const res = await api.put("/notifications/mark-all-read");
+  return res.data;
+};
